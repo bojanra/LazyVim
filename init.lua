ce142edbac4eb2fc78895e5c3d160e1cd7fdc19d
Bojan Ramšak <bojan.ramsak@rtvslo.si>
2023-05-01 19:50:12 +0200

    Additional shortcuts for moving lines or blocks

--- init.lua
+++ init.lua
@@ -1,19 +1,59 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- set titlestring=%t%(\ %M%)%(\ (%{expand(\"%:~:.:h\")})%)%(\ %a%)
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("i", "hh", "=>", opts)
+keymap("i", "kk", "->", opts)
+keymap("i", "kkk", "->{}<ESC>i", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Insert --
+-- Press jk fast to exit insert mode
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+vim.filetype.add({
+  pattern = {
+    [".*%.html%.ep"] = "html",
+  },
+})
 
 vim.opt.title = true
 vim.opt.titlestring = [[%f %h%m%r%w %{v:progname} (%{tabpagenr()} of %{tabpagenr('$')})]]
-
--- vim.cmd("colorscheme kanagawa-wave")
+-- vim.cmd("set nospell")
 
 vim.g.tokyodark_transparent_background = false
 vim.g.tokyodark_enable_italic_comment = true
 vim.g.tokyodark_enable_italic = true
 vim.g.tokyodark_color_gamma = "1.0"
--- vim.cmd("colorscheme tokyodark")
-vim.cmd.colorscheme("sweetie")
+vim.cmd("colorscheme tokyonight-storm")
 
 require("lualine").setup({
   tabline = {
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
-  --  { "rebelot/kanagawa.nvim" },
-  --  { "tiagovla/tokyodark.nvim", },
+  -- { "rebelot/kanagawa.nvim" },
+  -- { "tiagovla/tokyodark.nvim", },
+  { "catppuccin/nvim", lazy = true, name = "catppuccin", },
+  -- { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin", }, },
   {
     "NTBBloodbath/sweetie.nvim",
     config = function()
